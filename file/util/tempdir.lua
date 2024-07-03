5ef3136b799caac81776d7afa89453d1ca8a978a
Your Name <you@example.com>
2024-07-03 16:43:02 +0300

    update

--- file/util/tempdir.lua
+++ file/util/tempdir.lua
@@ -2,20 +2,19 @@ local File_Util_Tempdir = {}
 get_os_name = require 'get_os_name'
 lfs = require 'lfs'
 unistd = require 'posix.unistd'
-bit = require 'bit32'
+--bit = require 'bit32'
 -- DATE
 -- VERSION
-local jit_os = get_os_name()
 
 local function get_tempdir()
-    if jit_os == 'Windows' then
+    if get_os_name() == 'Windows' then
         for _, env_var in ipairs{'TMP', 'TEMP', 'TMPDIR', 'TEMPDIR'} do
             if os.getenv(env_var) then
                 return os.getenv(env_var)
             end
         end
         for _, dir in ipairs{'C:\\TMP', 'C:\\TEMP'} do
-            if lfs.attributes(dir, 'mode') == 'directory' then
+            if lfs.attributes(dir).mode == 'directory' then
                 return dir
             end
         end
@@ -26,7 +25,7 @@ local function get_tempdir()
             end
         end
         for _, dir in ipairs{'/tmp', '/var/tmp'} do
-            if lfs.attributes(dir, 'mode') == 'directory' then
+            if lfs.attributes(dir).mode == 'directory' then
                 return dir
             end
         end
@@ -35,25 +34,36 @@ local function get_tempdir()
 end
 
 local function get_user_tempdir()
-    if jit_os == 'Windows' then
+    if get_os_name() == 'Windows'
+    then
         return get_tempdir()
     else
-        local dir = os.getenv('XDG_RUNTIME_DIR') or get_tempdir()
-        local info = {lfs.attributes(dir)}
-        if not info then
-            error("Can't stat tempdir '" .. dir .. "': " .. errno())
+        local uid = unistd.geteuid()
+        local dir = os.getenv('XDG_RUNTIME_DIR')
+        if dir
+        then
+            local info = lfs.attributes(dir)
+            if info
+            then
+                if (info.uid==uid) and (info.permissions:sub(0,3)=='rwx')
+                then
+                    return dir
+                end
+            end
         end
-        if info[5] == unistd.geteuid() and bit.band(info[3], 0x022) == 0 then
-            return dir
-        end
+
+        dir = get_tempdir()
         local i = 0
         while true do
-            local subdir = dir .. '/' .. unistd.geteuid() .. (i > 0 and ('.' .. i) or '')
-            local subinfo = {lfs.attributes(subdir)}
-            if not subinfo then
-                lfs.mkdir(subdir, 0x700)
+            local subdir = dir..'/'..uid..(i>0 and ('.'..i) or '')
+            print(subdir)
+            local subinfo = lfs.attributes(subdir)
+            if not subinfo
+            then
+                lfs.mkdir(subdir)
                 return subdir
-            elseif subinfo[5] == unistd.geteuid() and bit.band(subinfo[3], 0x022) == 0 then
+            elseif (subinfo.uid==uid) and (subinfo.permissions:sub(0,3)=='rwx')
+            then
                 return subdir
             else
                 i = i + 1
